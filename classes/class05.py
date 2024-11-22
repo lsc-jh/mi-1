05bf44d9494217f2acd5b4453a4c9f0da7da5d2c
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-22 16:58:23 +0100

    added AI parts

--- classes/class05.py
+++ classes/class05.py
@@ -1,16 +1,33 @@
-# FastAPI example
-import re
-from typing import Annotated, Literal
-from fastapi import FastAPI, Query
+from typing import Literal
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-app = FastAPI()
+from chatbot import Chat, register_call
+import wikipedia
+
+from transformers import file_utils
+print(file_utils.default_cache_path)
+
+exit()
+
+# Just example :-)
+tags_metadata = [
+    {
+        "name": "calculate",
+        "description": "Operations to calculate numbers."
+    }
+]
+
+app = FastAPI(
+    openapi_tags=tags_metadata
+)
+
 
 @app.get("/")
 def welcome():
     return {"status": "up"}
 
-@app.get("/calculate")
+@app.get("/calculate", tags=["calculate"])
 def calculate(a: int, b: int):
     """This function adds two numbers togehter."""
     result = a + b
@@ -21,7 +38,7 @@ def calculate(a: int, b: int):
         "result": result
     }
 
-@app.get("/calculate/{a}/{b}")
+@app.get("/calculate/{a}/{b}", tags=["calculate"])
 def calculate_with_path(a: int, b: int, operator: Literal["+", "-", "*", "/"] = "+"):
     """ABC"""
 
@@ -53,10 +70,7 @@ class Request(BaseModel):
     b: int
     operation: str = "+"
 
-class Operation(BaseModel):
-    message: str
-
-@app.post("/calculate")
+@app.post("/calculate", tags=["calculate"])
 def calculate_post(req: Request):
     a, b, operator = req.a, req.b, req.operation
 
@@ -80,6 +94,15 @@ def calculate_post(req: Request):
     }
 
 
+class Operation(BaseModel):
+    message: str
 
+@app.post("/ai/chat")
+def ai_chat(op: Operation):
+    text = op.message
+    if text is None or text == "":
+        raise HTTPException(status_code=418, detail="Message is empty!")
+    chat = Chat()
+    return chat.respond(text)
 
 
